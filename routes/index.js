997a6a3edfaef5aa82adf57721f4c1b2db6b3b7c
Fer Sánchez <sangarfer@gmail.com>
2022-02-12 20:41:05 +0100

    lab done (styled by hand)

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require('../models/Movie.model');
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movie/:id", (req, res)=>{
+    const id = req.params.id;
+
+    MovieModel.findById(id)
+    .then((movie)=>{res.render("details.hbs", movie)})
+    .catch((err)=> console.log(`DB error reading '/movie/:id': ${err}`))
+})
+
+router.get("/movies", (req, res)=>{
+    MovieModel.find()
+    .then((movies)=>{res.render("movies.hbs", {movies})})
+    .catch((err)=> console.log(`DB error reading '/movies': ${err}`))
+})
+
+router.get('/', (req, res) => res.render('index'));
 
 module.exports = router;
